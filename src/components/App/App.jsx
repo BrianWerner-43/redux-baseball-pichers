0f7c1dfc1edc7d9832f384c2841d7f420c58722f
Brian Werner <brianmw199@gmail.com>
2023-11-28 22:43:12 -0600

    I tried my best at refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,23 @@
 import {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import TotalPitchers from '../TotalPitchers/TotalPitchers';
 
 function App() {
+
+  const dispatch = useDispatch();
   const [currentPitcher, setCurrentPitcher] = useState('Maud Nelson');
   const [currentCatcher, setCurrentCatcher] = useState('Elston Howard');
 
-  const [pitcherList, setPitcherList] = useState(['Maud Nelson', 'Ila Borders', 'Don Newcombe', 'CC Sabathia']);
+  
   const [catcherList, setCatcherList] = useState(['Roy Campanella', 'Elston Howard', 'Kenji Jojima']);
-  const [newPitcher, setNewPitcher] = useState('');
+ 
   const [newCatcher, setNewCatcher] = useState('');
 
-  const handlePitcherNameChange = event => {
-    setNewPitcher(event.target.value);
-  };
+  
+  
 
-  // add new pitcher to the array. this will move to the pitcher reducer!
-  const handlePitcherSubmit = event => {
-    event.preventDefault();
-    // spread: give me everything in pitcherList, then add this new thing
-    setPitcherList([...pitcherList, newPitcher]);
-    setNewPitcher('');
-  };
+    
+   
 
   const handleCatcherNameChange = event => {
     setNewCatcher(event.target.value);
@@ -28,6 +26,13 @@ function App() {
   // add new catcher to array. this will move to the catcher reducer!
   const handleCatcherSubmit = event => {
     event.preventDefault();
+
+    dispatch ({
+      type: 'OUR_CATCHERS',
+      payload: newCatcher
+    })
+
+
     // spread: give me everything in catcherList, then add this new thing
     setCatcherList([...catcherList, newCatcher]);
     setNewCatcher('');
@@ -38,18 +43,10 @@ function App() {
       <h1>Redux Baseball Pitchers</h1>
       <h2>On the Mound: {currentPitcher}</h2>
       <h2>Behind the Plate: {currentCatcher}</h2>
-      <div>Total Pitchers: {pitcherList.length}</div>
+      <TotalPitchers />
       <div>Total Catchers: {catcherList.length}</div>
       <h3>All Pitchers</h3>
-      <form onSubmit={handlePitcherSubmit}>
-        <input
-          type="text"
-          value={newPitcher}
-          onChange={handlePitcherNameChange}
-          placeholder="New Pitcher Name"
-        />
-        <button type="submit">Add Pitcher</button>
-      </form>
+      
       <ul>
         {pitcherList.map(pitcher => (
           <li
--- src/index.js
+++ src/index.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+// Create reducers
+const pitchers = (state = [], action) => {
+    if(action.type === 'OUR_PITCHERS') {
+       const newPitcher = action.payload
+       return [...state, newPitcher] 
+    }
+    return state
+}
+
+const catchers = (state = [], action) => {
+    if (action.type === 'OUR_CATCHERS') {
+        const newCatcher = action.payload
+        return [...state, newCatcher]
+    }
+    return state
+}
+
+
+
+
+// create the store
+const baseBall = createStore (
+    combineReducers({
+        pitchers,
+        catchers
+    })
+)
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={baseBall}>
     <React.StrictMode>
         <App />
     </React.StrictMode>
+     </Provider>
 );
